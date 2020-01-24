b85e1be5d079cfd0692a6e504ad4dd90bcd545f3
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2020-01-23 23:57:14 -0500

    Add files via upload

---
+++ tarea1final.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingrese un cateto");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro cateto");
+            double b = double.Parse(Console.ReadLine());
+            double c = Math.Sqrt((a * a + b * b));
+            Console.WriteLine("hipotenusa=" + c);
+
+            double d = Math.Asin(a / c);
+            double e = d * (Math.PI / 180.0);
+            Console.WriteLine("el ángulo e es igual a:" + e);
+
+            double f = Math.Asin(b / c);
+            double g = f * (Math.PI / 180.0);
+            Console.WriteLine("el ángulo g es igual a:" + g); 
+            
+
+
+
+
+
+)
+        }
+    }
+}
