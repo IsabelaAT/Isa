3372ae33a5d5cbb109927f5d6065dab6a7735d0b
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2020-01-24 10:37:26 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EejemploClase
+namespace tarea2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("ingrese x1: ");
-            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserte un ángulo menor a 90");
+            double a = double.Parse(Console.ReadLine());
+            double b = a * (Math.PI / 180.0);
 
-            Console.Write("ingrese x2: ");
-            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserte hipotenusa");
+            double t = double.Parse(Console.ReadLine());
 
-            Console.Write("ingrese y1: ");
-            double y1 = double.Parse(Console.ReadLine());
+            double c = (90 - b);
+            Console.WriteLine("el ángulo c es igual a:" + c);
+        
+            double z = (t * Math.Sin c);
+            Console.WriteLine("el lado z es igual a:" + z);
+            double y = ( t * Math.Cos c);
+            Console.WriteLine("el lado y es igual a:");
 
-            Console.Write("ingrese y2: ");
-            double y2 = double.Parse(Console.ReadLine());
 
-            double m = (y2 - y1) / (x2 - x1);
-            double b = y1 - (m * x1);
-            double d = Math.Sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-
-            Console.WriteLine("su pendiente es: " + m + "el intercepto es: " + b + "la distancia es: " + d);
-
-            
-            
-            
-    
 
         }
     }
