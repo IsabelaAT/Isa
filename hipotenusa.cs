c1268b5e6036da3f1408a8cd27ba991ec4289417
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2020-01-23 23:55:12 -0500

    Add files via upload

---
+++ hipotenusa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("ingrese un cateto");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro cateto");
+            double b = double.Parse(Console.ReadLine());
+            double c = Math.Sqrt((a * a + b * b));
+                Console.WriteLine("hipotenusa=" + c);
+        }
+    }
+}
