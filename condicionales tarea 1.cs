fa822e9bb353db18eb8f9533d616f430f5299eec
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2020-01-30 18:56:21 -0500

    Add files via upload

---
+++ condicionales tarea 1.cs
@@ -0,0 +1,26 @@
+﻿  using System;
+
+namespace condicionales_tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingreso de datos
+            Console.WriteLine("ingrese su salario mensual");
+            double salario = double.Parse(Console.ReadLine());
+
+            // Cálculo de la tarifa
+            if (salario < 1755.803) { Console.WriteLine("usted es tarifa A"); }
+
+            else if (1755.803 <= salario && salario <= 3511.212)
+            {
+                Console.WriteLine("usted es tarifa B");
+            }
+            else (salario > 1755.803){ 
+                     
+                Console.WriteLine("usted es tarifa C");
+            }
+        }
+
+    } }
