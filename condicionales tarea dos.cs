122a6e4cc10d65bee4f4ea6b1c348c8f6f64f0d7
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2020-01-30 18:55:26 -0500

    Add files via upload

---
+++ condicionales tarea dos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace condicionales_tarea_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingreso de datos
+            Console.WriteLine("Ingrese su salario");
+            double salario = double.Parse(Console.ReadLine());
+
+           // Cálculo de la tarifa
+            if (salario < 1755606)
+            {
+                Console.WriteLine("su tarifa es A y debe pagar 3.400 pesos");
+            }
+            else if (1755606 <= salario && salario <= 4389015)
+            {
+                Console.WriteLine("su tarifa es B y debe pagar 13.500 pesos");
+            }
+            else { Console.WriteLine("su tarifa es C y debe pagar 33.600 pesos"); }
+
+        }
+    }
+}
